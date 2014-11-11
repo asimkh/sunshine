72155a3eb04a327544cfc443b67625502419d875
Asim Kh <asimkh@hotmail.com>
2014-11-11 14:06:02 +0400

    v2.1111.0

--- sunShine/app/src/main/java/com/asimkh/sunshine/sunshine/MainActivity.java
+++ sunShine/app/src/main/java/com/asimkh/sunshine/sunshine/MainActivity.java
@@ -6,13 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -21,6 +15,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         Button WeatherBtn = (Button) findViewById(R.id.weatherButton);
 
--- sunShine/app/src/main/java/com/asimkh/sunshine/sunshine/payment.java
+++ sunShine/app/src/main/java/com/asimkh/sunshine/sunshine/payment.java
@@ -1,18 +1,116 @@
 package com.asimkh.sunshine.sunshine;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+
+import static android.view.View.OnClickListener;
 
 
 public class payment extends Activity {
+
+    private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_PRODUCTION;
+    private static final String CONFIG_CLIENT_ID = "AVgrmxBcJsBvrymRzZOGPNsKSQfD4-EhqB79h0ValhkQzw8M3AEqicl2yy6E";
+    private static final int REQUEST_PAYPAL_PAYMENT = 100;
+
+    private static PayPalConfiguration configPayPyal = new PayPalConfiguration()
+            .environment(CONFIG_ENVIRONMENT)
+            .clientId(CONFIG_CLIENT_ID);
+            //.merchantName("SunShine App");
+            //.merchantPrivacyPolicyUri(Uri.parse("http://"))
+            //.merchantUserAgreementUri()
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+
+
+
+
+
+
+         /* call PayPal Services */
+
+        Intent initPayPal = new Intent(this, PayPalService.class);
+        initPayPal.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, configPayPyal);
+        startService(initPayPal);
+
+        /* Paypayment button */
+
+        Button PaymentB1 = (Button) findViewById(R.id.PayPalButton1);
+        PaymentB1.setOnClickListener(new OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            PayPalPayment thingToBuy = new PayPalPayment(new BigDecimal(1), "USD", "SunShine App", PayPalPayment.PAYMENT_INTENT_SALE);
+            Intent intentPayPal = new Intent(payment.this, PaymentActivity.class);
+
+            intentPayPal.putExtra(PaymentActivity.EXTRA_PAYMENT, thingToBuy);
+            startActivityForResult(intentPayPal, REQUEST_PAYPAL_PAYMENT);
+                }
+
+            });
+
+        /* ------- */
+
+
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PAYPAL_PAYMENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                PaymentConfirmation confirm = data
+                        .getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirm != null) {
+                    try {
+                        System.out.println("Responseeee"+confirm);
+                        Log.i("paymentExample", confirm.toJSONObject().toString());
+
+
+                        JSONObject jsonObj=new JSONObject(confirm.toJSONObject().toString());
+
+                        String paymentId=jsonObj.getJSONObject("response").getString("id");
+                        System.out.println("payment id:-=="+paymentId);
+                        Toast.makeText(getApplicationContext(), paymentId, Toast.LENGTH_LONG).show();
+
+                    } catch (JSONException e) {
+                        Log.e("paymentExample", "an extremely unlikely failure occurred: ", e);
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Log.i("paymentExample", "The user canceled.");
+            } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+                Log.i("paymentExample", "An invalid Payment was submitted. Please see the docs.");
+            }
+        }
+
+
+    }
+
+
+
 
 
     @Override
@@ -36,4 +134,6 @@ public class payment extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
